b2bb9a67b614c768f4128921cd1d883a143d0d1a
HLed12 <HLeduc@clarku.edu>
2024-02-15 05:14:57 +0000

    Add function 'my_calc' and vignette 'module3'. Updates NAMESPACE. Renames 'my_first_vignette' to 'module1'

---
+++ R/my_calc.R
@@ -0,0 +1,31 @@
+#' A calculator function
+#'
+#' @description This function allows you to multiply, divide, add, or
+#' subtract two inputted numbers.
+#' @param x A vector or matrix
+#' @param y A vector or matrix
+#' @param fun A string
+#' @export
+#' @examples
+#' x <- 1:5
+#' y <- c(5, 2, 3, 10, 2)
+#' fun <- "divide"
+#' my_calc(x, y, "plus")
+#' my_calc(x, y, "subtract")
+#' my_calc(x, y, fun)
+#' my_calc(x, y, "multiply")
+my_calc <- function(x, y, fun) {
+  if(fun == "sum") {
+    o <- x + y
+  } else if(fun == "subtract") {
+    o <- x - y
+  } else if(fun == "multiply") {
+    o <- x * y
+  } else if(fun == "divide") {
+    o <- x / y
+  } else {
+    o <- paste("Invalid operation. Choose between 'sum', 'subtract'," ,
+               "'multiply', and 'divide'.")
+  }
+  return(o)
+}
